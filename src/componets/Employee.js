775f437aef73f189e441082911b98f7c241d818d
Burcu Saglam <saglamburcu96@gmail.com>
2022-03-10 12:59:59 +0300

    transferred the employee data with the useState hooks

--- src/componets/Employee.js
+++ src/componets/Employee.js
@@ -1,16 +1,22 @@
-const Employee = () => {
+const Employee = ({ employees }) => {
   return (
-    <tr>
-      <td>Thomas Hardy</td>
-      <td>thomashardy@mail.com</td>
-      <td>89 Chiaroscuro Rd, Portland, USA</td>
-      <td>(171) 555-2222</td>
-      <td>
-        <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-        <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-      </td>
-    </tr>
-  )
+    employees.map(employee => (
+
+      <tr>
+        <td>{employee.name}</td>
+        <td>{employee.email}</td>
+        <td>{employee.address}</td>
+        <td>{employee.phone}</td>
+        <td>
+          <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+          <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+        </td>
+      </tr>
+
+    )
+    )
+
+  );
 }
 
 export default Employee;
