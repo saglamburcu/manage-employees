5fba8af0200cedf3de1d27f7902f248b9fb83d68
Burcu Saglam <saglamburcu96@gmail.com>
2022-03-10 13:55:32 +0300

    EmployeeContext was created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import EmployeeList from "./componets/EmployeeList";
+import EmployeeContextProvider from "./context/EmployeeContext";
 
 const App = () => {
   return (
-    <div class="container-xl">
-      <div class="table-responsive">
-        <div class="table-wrapper">
-          <div class="table-title">
-            <div class="row">
-              <div class="col-sm-6">
+    <div className="container-xl">
+      <div className="table-responsive">
+        <div className="table-wrapper">
+          <div className="table-title">
+            <div className="row">
+              <div className="col-sm-6">
                 <h2>Manage <b>Employees</b></h2>
               </div>
-              <div class="col-sm-6">
-                <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
+              <div className="col-sm-6">
+                <a href="#addEmployeeModal" className="btn btn-success" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
               </div>
             </div>
           </div>
-          <EmployeeList />
+          <EmployeeContextProvider>
+            <EmployeeList />
+          </EmployeeContextProvider>
         </div>
       </div>
     </div>
--- src/componets/Employee.js
+++ src/componets/Employee.js
@@ -1,15 +1,16 @@
 const Employee = ({ employees }) => {
+
   return (
     employees.map(employee => (
 
-      <tr>
+      <tr key={employee.id}>
         <td>{employee.name}</td>
         <td>{employee.email}</td>
         <td>{employee.address}</td>
         <td>{employee.phone}</td>
         <td>
-          <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-          <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+          <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+          <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
         </td>
       </tr>
 
